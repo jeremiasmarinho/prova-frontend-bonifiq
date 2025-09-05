ce59842a449ed7c1285786ec5697a08be6cbbcdd
jeremiasmarinho <jeremias10@msn.com>
2025-09-05 05:16:11 -0300

    Responsivo concluído

--- widget-app/src/main.tsx
+++ widget-app/src/main.tsx
@@ -2,6 +2,7 @@ import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import "./styles/responsive.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
