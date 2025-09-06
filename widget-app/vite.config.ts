dd8a3f6a61f6ca07face26547504df1524771cd0
jeremiasmarinho <jeremias10@msn.com>
2025-09-05 23:19:39 -0300

    Criação do Build

--- widget-app/vite.config.ts
+++ widget-app/vite.config.ts
@@ -3,10 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: "jsdom",
-    setupFiles: "src/test/setup.ts",
-    globals: true,
-    css: true,
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
 });
